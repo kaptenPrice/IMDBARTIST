81a15655af4b4a32e75683311ef6d1bf9f0486e6
81khameh <khazar.mehraban@gmail.com>
2020-06-01 21:47:18 +0200

    removed Body and ButtonBuidlder components

--- src/imdbRandom.js
+++ src/imdbRandom.js
@@ -3,22 +3,13 @@ import "./imdbRandom.css"
 import ButtonComponent from "./components/ButtonComponent";
 import actorsData from "./data/imdb.json";
 import CardComponent from "./components/Card";
-const title = "IMDB APP";
 
-const buttonTextList = ["Add random actor", "Sort By Name", "Sort By popularity"];
+const buttonTextList = ["Generate random actors", "Sort By Name", "Sort By popularity"];
 
 
 
 class ImdbRandom extends Component {
-    constructor (props){
-        super(props)
-        this.state={
-            newObjcetIsGenerate :new Set(),
-            result:generateRandomActors(),
 
-        };
-        
-    }
    
 
     render() {
@@ -26,9 +17,7 @@ class ImdbRandom extends Component {
         return (
             <div className="ImdbRandom">
 
-                <Body title={title} />
-                <ButtonBuilder buttonProps={buttonTextList} />
-                <List content={generateRandomActors()} />
+                <List content={generateRandomActors()} buttonProps={buttonTextList} />
 
             </div>
         );
@@ -36,36 +25,56 @@ class ImdbRandom extends Component {
 
 }
 
-const generateRandomActors = () => {
+ const generateRandomActors =()=> {
+    
     let result = [];
     for (let i = 0; i < 5; i++) {
         let random = Math.floor(Math.random() * actorsData.length);
         result.push(actorsData[random]);
-        actorsData.splice(random, 1);
+      /*  actorsData.splice(random, 1);*/
     }
     return result;
 }
 
-class Body extends Component {
-    render() {
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
-}
+
+
+
 
 class List extends Component {
     delete(actorName) {
         const copyArr=Object.assign([], this.props.content);       
         console.log("Name deleted " + actorName);
-        copyArr.splice(actorName,1);
-        
+        copyArr.splice(actorName,1);  
+    }
+    sortByName(){
+        alert("Sorting by Name")
+    }
+    sortByPop(){
+        alert("Sorted by popularity")
     }
 
     render() {
         return (
+           <div> 
+           <h1>Imdb App</h1>
+            <div>
+            <ButtonComponent 
+            key={this.props.buttonProps[0]}
+            buttonText={this.props.buttonProps[0]}
+            onClickFunction={console.log("Generate new list")}
+            />
+            <ButtonComponent 
+            key={this.props.buttonProps[1]}
+            buttonText={this.props.buttonProps[1]}
+            onClickFunction={this.sortByName}
+            /> 
+            <ButtonComponent 
+            key={this.props.buttonProps[2]}
+            buttonText={this.props.buttonProps[2]}
+            onClickFunction={this.sortByPop}
+            />
+                          
+            </div>
             <table>
                 <tbody>
                     {this.props.content.map((elem) =>
@@ -76,18 +85,19 @@ class List extends Component {
                             buttonOnClickFunction={()=>this.delete(elem.name)} />
                             )}
                 </tbody>
-            </table>)
+            </table>
+            </div>
+            )
 
     }
 }
-class ButtonBuilder extends Component {
-    render() {
-        return (
-            <div>
-                {this.props.buttonProps.map((elem) => <ButtonComponent key={elem} buttonText={elem} onClickFunction={generateRandomActors} />)}
-            </div>
-        )
-    }
-}
+
 
 export default ImdbRandom;
+/*  {this.props.buttonProps.map((elem) => 
+                    <ButtonComponent 
+                    key={elem}
+                    buttonText={elem}
+                    onClickFunction={} 
+                    />
+                    )}*/
