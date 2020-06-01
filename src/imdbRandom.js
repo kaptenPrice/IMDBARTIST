a04db3ecb60cd22ec44c815e1fba4e11114e7e17
81khameh <khazar.mehraban@gmail.com>
2020-06-01 21:02:19 +0200

    deletebuttonsAdded

--- src/imdbRandom.js
+++ src/imdbRandom.js
@@ -1,24 +1,25 @@
 import React, { Component } from "react";
+import "./imdbRandom.css"
 import ButtonComponent from "./components/ButtonComponent";
 import actorsData from "./data/imdb.json";
 import CardComponent from "./components/Card";
-const title = "IMDB APP"
+const title = "IMDB APP";
 
 const buttonTextList = ["Add random actor", "Sort By Name", "Sort By popularity"];
 
 
-const generateRandomActor = () => {
-    let result = [];
-    for (let i = 0; i < 5; i++) {
-        let random = Math.floor(Math.random() * actorsData.length);
-        result.push(actorsData[random]);
-        actorsData.splice(random, 1);
-
-    }
-    return result;
-}
 
 class ImdbRandom extends Component {
+    constructor (props){
+        super(props)
+        this.state={
+            newObjcetIsGenerate :new Set(),
+            result:generateRandomActors(),
+
+        };
+        
+    }
+   
 
     render() {
 
@@ -27,13 +28,24 @@ class ImdbRandom extends Component {
 
                 <Body title={title} />
                 <ButtonBuilder buttonProps={buttonTextList} />
-                <List content={generateRandomActor()} />
+                <List content={generateRandomActors()} />
 
             </div>
         );
     }
 
 }
+
+const generateRandomActors = () => {
+    let result = [];
+    for (let i = 0; i < 5; i++) {
+        let random = Math.floor(Math.random() * actorsData.length);
+        result.push(actorsData[random]);
+        actorsData.splice(random, 1);
+    }
+    return result;
+}
+
 class Body extends Component {
     render() {
         return (
@@ -46,8 +58,10 @@ class Body extends Component {
 
 class List extends Component {
     delete(actorName) {
-        console.log("Name borta " + actorName);
-
+        const copyArr=Object.assign([], this.props.content);       
+        console.log("Name deleted " + actorName);
+        copyArr.splice(actorName,1);
+        
     }
 
     render() {
@@ -70,7 +84,7 @@ class ButtonBuilder extends Component {
     render() {
         return (
             <div>
-                {this.props.buttonProps.map((elem) => <ButtonComponent key={elem} buttonText={elem} />)}
+                {this.props.buttonProps.map((elem) => <ButtonComponent key={elem} buttonText={elem} onClickFunction={generateRandomActors} />)}
             </div>
         )
     }
